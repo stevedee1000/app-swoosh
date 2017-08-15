55fc781859ebcc99064a80c324cbe86c6ac44657
stevedee1000 <stevedee1000@sbcglobal.net>
2017-08-15 08:35:34 -0700

    Lesson-07 completed

--- app-swoosh/app-swoosh/Controller/WelcomeVC.swift
+++ app-swoosh/app-swoosh/Controller/WelcomeVC.swift
@@ -8,23 +8,16 @@
 
 import UIKit
 
-class WelcomeVC: UIViewController {    
-    @IBOutlet weak var swoosh: UIImageView!
-    @IBOutlet weak var bgImg: UIImageView!
+class WelcomeVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
     }
 
     @IBAction func unwindFromSkillVC (unwindSegue: UIStoryboardSegue) {
         
     }
-
-        
-    
-
 
 }
 
