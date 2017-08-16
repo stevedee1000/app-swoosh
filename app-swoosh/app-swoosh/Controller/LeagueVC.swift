c058e701b730abf5b6cd7e08bae59a9679088a91
stevedee1000 <stevedee1000@sbcglobal.net>
2017-08-15 18:23:31 -0700

    Lesson-10 completed

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -42,5 +42,11 @@ class LeagueVC: UIViewController {
         nextBtn.isEnabled = true
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let skillVC = segue.destination as? SkillVC {
+            skillVC.player = player
+        }
+    }
+    
 }
 
